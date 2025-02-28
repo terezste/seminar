bcdc30b6fcbbf28d132f9df367483bb05ece2fbd
tereza <terezaste178@gmail.com>
2025-02-28 11:27:10 +0100

    28. 2. lesson + work

---
+++ 23 - 28.2.2025/GraphPlayground/GraphPlayground/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+ * Made by Jan Borecky for PRG seminar at Gymnazium Voderadska, year 2024-2025.
+ * Extended by students.
+ */
+
+namespace GraphPlayground
+{
+    internal class Program
+    {
+        public static void DFS(Graph graph, Node startNode, Node targetNode = null)
+        {
+            /*
+            Console.WriteLine("Starting node: " + startNode.index);
+            bool shouldContinue = true;
+
+            while (shouldContinue)
+            {
+                foreach (Node startNeighbor in startNode.neighbors)
+                {
+                    if (!startNeighbor.visited)
+                    {
+                        shouldContinue = true;
+                        break;
+                    }
+                }
+            }*/
+        }
+
+        public static void BFS(Graph graph, Node startNode, Node targetNode = null)
+        {
+            
+            Console.WriteLine("Starting node: " + startNode.index);
+            Node currentNode = null;
+            List <Node> queue = new List <Node>() { startNode };
+
+            while (queue.Count > 0)
+            {
+                currentNode = queue[0];
+                currentNode.visited = true;
+                Console.WriteLine("Current node: " + currentNode.index);
+                queue.RemoveAt(0);
+                foreach(Node neighbor in currentNode.neighbors)
+                {
+                    if (!neighbor.visited)
+                    {
+                        queue.Add(neighbor);
+                        Console.WriteLine("   Adding node " + neighbor.index + " to queue.");
+                    }
+                    else Console.WriteLine("   Node " + neighbor.index + " is already visited.");
+                }
+            }
+            Console.WriteLine("Finished at node " + currentNode.index);
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            //Create and print the graph
+            Graph graph = new Graph();
+            graph.PrintGraph();
+            graph.PrintGraphForVisualization();
+
+            //Call both algorithms with a random starting node
+            Random rng = new Random();
+            DFS(graph, graph.nodes[rng.Next(0, graph.nodes.Count)]);
+            BFS(graph, graph.nodes[rng.Next(0, graph.nodes.Count)]);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Game/Game/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class Program
+    {
+        static char Deciding2Options (string optionA, string optionB)
+        {
+            //Rozhodovani se mezi dvemi moznostmi a kontrola vstupu
+
+            Console.WriteLine("\na) " + optionA);
+            Console.WriteLine("b) " + optionB);
+
+            while (true)
+            {
+                char decision = char.ToLower(Console.ReadKey().KeyChar);
+
+                if (decision == 'a')
+                {
+                    return decision;
+                }
+                else if (decision == 'b')
+                {
+                    return decision;
+                }
+                else
+                {
+                    Console.WriteLine("\nInvalid input. Enter a, or b");
+                    continue;
+                }
+            }
+        }
+
+        static char Deciding3Options(string optionA, string optionB, string optionC)
+        {
+            //Rozhodovani se mezi tremi moznostmi a kontrola vstupu
+
+            Console.WriteLine("\na) " + optionA);
+            Console.WriteLine("b) " + optionB);
+            Console.WriteLine("c) " + optionC);
+
+            while (true)
+            {
+                char decision = char.ToLower(Console.ReadKey().KeyChar);
+
+                if (decision == 'a' || decision == 'b' || decision == 'c')
+                {
+                    return decision;
+                }
+                else
+                {
+                    Console.WriteLine("\nInvalid input. Enter a, b, or c");
+                    continue;
+                }
+            }
+        }
+
+        static void WaitForInput()
+        {
+            if
+            {
+                
+            }
+            else
+            {
+
+            }
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            string[] inventory;
+
+            Console.WriteLine();
+
+        }
+    }
+}
