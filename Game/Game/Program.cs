4204dff64c455730f3eeb3965996f06a3beb5335
tereza <terezaste178@gmail.com>
2025-02-28 14:39:06 +0100

    Game progress

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -8,6 +8,9 @@ namespace Game
 {
     internal class Program
     {
+        //tvorim si list inventory/inventar
+        static List<string> inventory = new List<string>();
+
         static char Deciding2Options (string optionA, string optionB)
         {
             //Rozhodovani se mezi dvemi moznostmi a kontrola vstupu
@@ -61,25 +64,61 @@ namespace Game
 
         static void WaitForInput()
         {
-            if
+            while (true)
             {
-                
-            }
-            else
-            {
+                Console.WriteLine();
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                //tady jsem si pomohla: https://chatgpt.com/share/67c1b454-02e8-8012-91b2-9a4b40b65eb8
 
+                if (keyInfo.Key == ConsoleKey.G)
+                {
+                    PrintGuide();
+                }
+                else if (keyInfo.Key == ConsoleKey.I)
+                {
+                    PrintInventory();
+                }
+                else if (keyInfo.Key == ConsoleKey.Enter || keyInfo.Key == ConsoleKey.Spacebar)
+                {
+                    break;
+                }
             }
         }
 
+        static void PrintGuide()
+        {
+            Console.WriteLine("zmáčkni Enter nebo mezerník pro pokračování dále\nzmáčkni 'a', 'b', 'c' pro výběr možnosti\nzmáčkni 'i' pro otevření inventáře\nzmáčkni 'g' pro otevření vodítka");
+        }
+
+        static void PrintInventory()
+        {
+            //vypise vse, co je v inventari
+
+            if (inventory.Count == 0)
+            {
+                Console.WriteLine("Nic tu není.");
+            }
+            else
+            {
+                foreach (string i in inventory)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
 
 
 
         static void Main(string[] args)
         {
 
-            string[] inventory;
+            /*PrintGuide();
 
-            Console.WriteLine();
+            PrintInventory();
+
+            Deciding2Options("dbajhb", "bdauib");
+
+            WaitForInput();*/
 
         }
     }
