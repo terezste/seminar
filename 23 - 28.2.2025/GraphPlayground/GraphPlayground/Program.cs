641a4922b60f82798b336737da22f9fd5f1105bc
tereza <terezaste178@gmail.com>
2025-03-07 09:21:05 +0100

    7.3.

--- 23 - 28.2.2025/GraphPlayground/GraphPlayground/Program.cs
+++ 23 - 28.2.2025/GraphPlayground/GraphPlayground/Program.cs
@@ -10,28 +10,55 @@ namespace GraphPlayground
 {
     internal class Program
     {
-        public static void DFS(Graph graph, Node startNode, Node targetNode = null)
-        {
-            /*
-            Console.WriteLine("Starting node: " + startNode.index);
-            bool shouldContinue = true;
 
-            while (shouldContinue)
+        public static void DFS1(Graph graph, Node startNode, Node targetNode = null)
+        {
+            //pozmeneny BFS
+            Console.WriteLine("Starting node: " + startNode.index);
+            Node currentNode = null;
+            List<Node> stack = new List<Node>() { startNode };
+
+            while (stack.Count > 0)
             {
-                foreach (Node startNeighbor in startNode.neighbors)
+                currentNode = stack[0];
+                currentNode.visited = true;
+                Console.WriteLine("Current node: " + currentNode.index);
+                stack.RemoveAt(0);
+                foreach (Node neighbor in currentNode.neighbors)
                 {
-                    if (!startNeighbor.visited)
+                    if (!neighbor.visited)
                     {
-                        shouldContinue = true;
-                        break;
+                        //queue.Add(neighbor);
+                        stack.Insert(0, neighbor); //jenom tohle se prida - basically vkladam do fronty ty neighbors, takze projedu do hloubky
+                        Console.WriteLine("   Adding node " + neighbor.index + " to queue.");
                     }
+                    else Console.WriteLine("   Node " + neighbor.index + " is already visited.");
                 }
-            }*/
+            }
+            Console.WriteLine("Finished at node " + currentNode.index);
+        }
+
+        public static void DFS2(Node currentNode)
+        {
+            //pomoci rekurze
+            Console.WriteLine("Current node: " + currentNode.index);
+            currentNode.visited = true;
+            foreach (Node neighbor in currentNode.neighbors)
+            {
+                if (!neighbor.visited)
+                {
+                    DFS2(neighbor);
+                }
+                else
+                {
+                    Console.WriteLine("   Neighbor " + neighbor.index + " already visited");
+                }
+            }
+            Console.WriteLine("Returning from " + currentNode.index);
         }
 
         public static void BFS(Graph graph, Node startNode, Node targetNode = null)
         {
-            
             Console.WriteLine("Starting node: " + startNode.index);
             Node currentNode = null;
             List <Node> queue = new List <Node>() { startNode };
@@ -53,8 +80,6 @@ namespace GraphPlayground
                 }
             }
             Console.WriteLine("Finished at node " + currentNode.index);
-
-
         }
 
         static void Main(string[] args)
@@ -66,7 +91,14 @@ namespace GraphPlayground
 
             //Call both algorithms with a random starting node
             Random rng = new Random();
-            DFS(graph, graph.nodes[rng.Next(0, graph.nodes.Count)]);
+
+            Console.WriteLine("\nDFS z BFS:");
+            DFS1(graph, graph.nodes[rng.Next(0, graph.nodes.Count)]);
+
+            Console.WriteLine("\nDFS s rekurzi:");
+            DFS2(graph.nodes[rng.Next(0, graph.nodes.Count)]);
+
+            Console.WriteLine("\nBFS:");
             BFS(graph, graph.nodes[rng.Next(0, graph.nodes.Count)]);
 
             Console.ReadKey();
